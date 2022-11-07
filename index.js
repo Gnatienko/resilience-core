091305811d23ecd7965cfe6e719163593bc65655
Gnatienko <gnatienko@gmail.com>
2022-11-07 19:41:57 +0200

    basic from lesson

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const express = require('express')
 const sequelize = require('./db')
 
 
-const PORT = process.env.PORT || 5000
 
 const app = express()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.status(200).json('!!!')
+  res.send('Hello World!')
 })
 
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (e) {
+      console.log(e)
+  }
 }
 
 
