ea4a60ceb3d67c1fd77230a39692c0648cc3ffb0
Gnatienko <gnatienko@gmail.com>
2022-11-15 18:35:06 +0200

    GPS removed

--- routes/gpsRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const gpsController = require('../controllers/gpsController')
-
-router.post('/', gpsController.create)
-router.get('/', gpsController.upDelGet)
-
-module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const Router = require('express')
 const router = new Router()
 const executorRouter = require('./executorRouter')
 const roleRouter = require('./roleRouter')
-const gpsRouter = require('./gpsRouter')
 
 router.use('/executor', executorRouter)
 router.use('/role', roleRouter)
-router.use('/gps', gpsRouter)
 
 module.exports = router
