7a7b65a35ddd03db62a6a1fb0c1851ceb70c1cba
Gnatienko <gnatienko@gmail.com>
2022-12-17 11:15:18 +0200

    execution to boolean

--- models/models.js
+++ models/models.js
@@ -1,28 +1,28 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
+const sequelize = require("../db")
+const { DataTypes } = require("sequelize")
 
-const Executor = sequelize.define('executor', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true},
+const Executor = sequelize.define("executor", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true },
 })
 
-const Role = sequelize.define('role', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true},
-    weight: {type: DataTypes.REAL},
+const Role = sequelize.define("role", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.REAL },
 })
 
-const ExecutorRole = sequelize.define('executor_role', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    qualification: {type: DataTypes.REAL},
-    execution: {type: DataTypes.REAL},
+const ExecutorRole = sequelize.define("executor_role", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  qualification: { type: DataTypes.REAL },
+  execution: { type: DataTypes.BOOLEAN },
 })
 
-Executor.belongsToMany(Role, {through: ExecutorRole })
-Role.belongsToMany(Executor, {through: ExecutorRole })
+Executor.belongsToMany(Role, { through: ExecutorRole })
+Role.belongsToMany(Executor, { through: ExecutorRole })
 
 module.exports = {
-    ExecutorRole,
-    Executor,
-    Role
+  ExecutorRole,
+  Executor,
+  Role,
 }
