f11d6ec2bcf1fba461388e4eebad81aadeb7dc2f
Gnatienko <gnatienko@gmail.com>
2022-12-17 11:55:31 +0200

    execution to boolean+

--- controllers/executorController.js
+++ controllers/executorController.js
@@ -1,66 +1,79 @@
-const {Executor} = require('../models/models')
-const {Role} = require('../models/models')
-const {ExecutorRole} = require('../models/models')
+const { Executor } = require("../models/models")
+const { Role } = require("../models/models")
+const { ExecutorRole } = require("../models/models")
 
-const { Op, where } = require('sequelize')
+const { Op, where } = require("sequelize")
 
 class ExecutorController {
+  async create(req, res) {
+    const { name } = req.query
+    const executor = await Executor.create({ name })
+    return res.json(executor)
+  }
 
-      
-    async create(req, res) {
-        const {name} = req.query
-        const executor = await Executor.create({name})
-        return res.json(executor)
+  async get(req, res) {
+    let { id } = req.query
+    let executor
+    if (id) {
+      executor = await Executor.findOne({ where: { id } })
+    } else {
+      executor = await Executor.findAll()
     }
+    return res.json(executor)
+  }
 
-    async get(req, res) {
-        let {id} = req.query
-        let executor
-        if(id) { executor = await Executor.findOne({where: {id} })}
-        else {executor = await Executor.findAll()}
-        return res.json(executor)
-        
+  async update(req, res) {
+    const { name, id } = req.query
+    const executor = await Executor.update({ name }, { where: { id } })
+    return res.json(executor)
+  }
+
+  async setSkill(req, res) {
+    const { executorId, roleId, qualification } = req.query
+    const executor = await Executor.findOne({ where: { id: executorId } })
+    const role = await Role.findOne({ where: { id: roleId } })
+    await executor.addRole(role, { through: { execution: 0, qualification } })
+    return res.json(executor)
+  }
+
+  async getSkill(req, res) {
+    const { executorId, roleId } = req.query
+    let executorSkills
+    if (roleId) {
+      executorSkills = await ExecutorRole.findOne({
+        where: { executorId, roleId, qualification: { [Op.gt]: 0 } },
+      })
+    } else {
+      executorSkills = await ExecutorRole.findAll({
+        where: { executorId, qualification: { [Op.gt]: 0 } },
+      })
     }
+    return res.json(executorSkills)
+  }
 
-    async update(req, res) {
-        const {name,id} = req.query
-        const executor = await Executor.update({name},{where: {id} })
-        return res.json(executor)
+  async setDuty(req, res) {
+    const { executorId, roleId, isDuty } = req.query
+    const executorRole = await ExecutorRole.findOne({
+      where: { executorId, roleId },
+    })
+    await executorRole.update({ isDuty: isDuty })
+    return res.json(executorRole)
+  }
+
+  async getDuty(req, res) {
+    const { executorId, roleId } = req.query
+    let executorDuties
+    if (roleId) {
+      executorDuties = await ExecutorRole.findOne({
+        where: { executorId, roleId, isDuty: "true" },
+      })
+    } else {
+      executorDuties = await ExecutorRole.findAll({
+        where: { executorId, isDuty: "true" },
+      })
     }
-
-    async setSkill(req, res) {
-        const {executorId,roleId,qualification} = req.query
-        const executor = await Executor.findOne({where: {id:executorId} })
-        const role = await Role.findOne({where: {id:roleId} })
-        await executor.addRole(role,{through: {execution:0, qualification}})
-        return res.json(executor)
-    }
-
-    async getSkill(req, res) { 
-        const {executorId, roleId} = req.query
-        let executorSkills
-        if(roleId) { executorSkills = await ExecutorRole.findOne({where: {executorId, roleId, qualification:{[Op.gt]:0}} })}
-        else {executorSkills = await ExecutorRole.findAll({ where:{executorId, qualification:{[Op.gt]:0}}})}
-        return res.json(executorSkills)
-    }
-
-    async setDuty(req, res) { 
-        const {executorId,roleId} = req.query
-        
-        const executorRole = await ExecutorRole.findOne({where:{executorId,roleId}})
-        const qualification = executorRole.qualification
-        await executorRole.update({execution:qualification})
-        return res.json(executorRole)
-    }
-
-    async getDuty(req, res) {
-        const {executorId, roleId} = req.query
-        let executorDuties
-        if(roleId) { executorDuties = await ExecutorRole.findOne({where: {executorId, roleId, execution:{[Op.gt]:0}} })}
-        else {executorDuties = await ExecutorRole.findAll({ where:{executorId, execution:{[Op.gt]:0}}})}
-        return res.json(executorDuties)
-    }
-
+    return res.json(executorDuties)
+  }
 }
 
 module.exports = new ExecutorController()
