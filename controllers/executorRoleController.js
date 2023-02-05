e552be82575643ff146870452fd7260ef85640da
Gnatienko <gnatienko@gmail.com>
2023-02-05 19:41:55 +0200

    hoursPerWeek

--- controllers/executorRoleController.js
+++ controllers/executorRoleController.js
@@ -5,11 +5,10 @@ const { ExecutorRole } = require("../models/models")
 //todo add assigned hours per week
 class ExecutorRoleController {
   async setSkill(req, res) {
-    const { executorId, roleId, qualification } = req.query
+    const { executorId, roleId, qualification, hoursPerWeek } = req.query
     const executor = await Executor.findOne({ where: { id: executorId } })
     const role = await Role.findOne({ where: { id: roleId } })
-    await executor.addRole(role, { through: { qualification } })
-
+    await executor.addRole(role, { through: { qualification, hoursPerWeek } })
     return res.status(200).send("Ok")
   }
 
